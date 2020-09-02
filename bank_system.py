5f440af8e60eb227978b5ca1f301e33bb78de0dc
Tim Aadland <potetstapp@gmail.com>
2020-09-02 13:44:04 +0200

    Added a database to store all the necessry information about the created card. SQLite has been used to create the database. The information that gets stored in it, is an id, card number, pin code and the balance.

--- bank_system.py
+++ bank_system.py
@@ -1,91 +1,141 @@
 import random
 import sys
+import sqlite3
 
-iin = "400000"
+conn = sqlite3.connect("card.s3db")
+# Cursor object
+cur = conn.cursor()
+# Execute the query
+sql_drop_card_table = """DROP TABLE card;"""
+cur.execute(sql_drop_card_table)
+conn.commit()
 card_info = {}
-balance = 0
-while True:
-    print("1. Create an account")
-    print("2. Log into account")
-    print("0. Exit")
-    user_input = int(input())
 
-    if user_input == 1:
-        sumer = 1
-        flag = True
-        while flag:
-            ran_card_num = str(random.randrange(100000000, 999999999))
-            iint_n = list(iin)
-            summing = list(ran_card_num)
-            begge = iint_n + summing
-            begge_int = [int(x) for x in begge]
-            intsumming = "".join([str(x) for x in begge])
+def create_sql_table():
+    "Creating a table called card"
+    sql_create_card_table = """CREATE TABLE IF NOT EXISTS card (
+                                    id integer PRIMARY KEY,
+                                    number text,
+                                    pin text,
+                                    balance integer DEFAULT 0 ); """
+    cur.execute(sql_create_card_table)
+    # When changes have been made in DB, remember to commit
+    conn.commit()
 
-            sumer = sum(begge_int)
-            if sumer % 10 != 0:
-                flag
+def luhm_algo_checker():
+    '''Using the luhm algorithm to check if a number is valid.
+    If the number pass the luhm algorithm, the number gets returned.
+    ALso generating a 4 digit number for the pin code, which also gets returned'''
+    iin = "400000"
+    sumer = 1
+    flag = True
+    while flag:
+        ran_card_num = str(random.randrange(100000000, 999999999))
+        iint_n = list(iin)
+        summing = list(ran_card_num)
+        begge = iint_n + summing
+        begge_int = [int(x) for x in begge]
+        intsumming = "".join([str(x) for x in begge])
 
-            pin_card_num = str(random.randint(0000, 9999))
-            org_num = intsumming
-            liste = list(org_num)
-            odd_multi_2 = [int(x) * 2 if i % 2 == 0 else int(x) for i, x in enumerate(liste)]
-            substract_9 = [x - 9 if x > 9 else x for x in odd_multi_2]
-            check_sum = sum(substract_9)
-            adder = 0
+        sumer = sum(begge_int)
+        if sumer % 10 != 0:
+            flag
 
-            if check_sum % 10 == 0:
-                check_sum = 0
-            while check_sum % 10 != 0:
-                check_sum += 1
-                adder += 1
-                continue
+        org_num = intsumming
+        liste = list(org_num)
+        odd_multi_2 = [int(x) * 2 if i % 2 == 0 else int(x) for i, x in enumerate(liste)]
+        substract_9 = [x - 9 if x > 9 else x for x in odd_multi_2]
+        check_sum = sum(substract_9)
+        adder = 0
 
-            full_ran_card = iin + ran_card_num + str(adder)
-            card_info[full_ran_card] = pin_card_num
-            num = ""
-            chekinger = pin_card_num
+        if check_sum % 10 == 0:
+            check_sum = 0
+        while check_sum % 10 != 0:
+            check_sum += 1
+            adder += 1
+            continue
 
-            for i in range(len(chekinger)):
-                if len(chekinger) != 4:
-                    num += '0'
-                    chekinger += "1"
-                else:
-                    break
+        full_ran_card = iin + ran_card_num + str(adder)
+        pin_card_num = str(random.randint(0000, 9999))
+        num = ""
+        temporary_pin = pin_card_num
 
-            pin_card_num = num + str(pin_card_num)
-            flag = False
+        for i in range(len(temporary_pin)):
+            if len(temporary_pin) != 4:
+                num += '0'
+                temporary_pin += "1"
+            else:
+                break
 
-        print("\nYour card has been created")
-        print(f"Your card number:\n{full_ran_card}")
-        print(f"Your card PIN:\n{pin_card_num}\n")
+        pin_card_num = num + str(pin_card_num)
+        card_info[full_ran_card] = pin_card_num
+        flag = False
 
-    elif user_input == 2:
-        print("\nEnter your card number:")
-        card_number = input()
-        print("Enter your PIN: ")
-        pin_number = input()
-        if card_number in card_info and card_info[
-            card_number] == pin_number:  # and card_info[card_number] == pin_number:
-            print("\nYou have successfully logged in!\n")
-            while True:
-                print("1. Balance")
-                print("2. Log out")
-                print("0. Exit")
-                login_success = int(input())
-                if login_success == 1:
-                    print(f"\nBalance: {balance}\n")
-                elif login_success == 2:
-                    print("\nYou have successfully logged out\n")
-                    break
-                elif login_success == 0:
-                    print("\nBye!")
-                    sys.exit()
+    return full_ran_card, pin_card_num
 
-        else:
-            print("Wrong card number or PIN!")
-            True
+def balance():
+    'Fetching the balance from the database corresponding to the account'
+    cur.execute(f"""SELECT 
+                        balance
+                    FROM
+                        card
+                    WHERE
+                        number = {number[0]} AND pin = {number[1]}""")
+    print(f"\nBalance: {cur.fetchone()[0]}")
 
-    elif user_input == 0:
-        print("\nBye!")
-        sys.exit()
+def logged_in():
+    'The menu after the user has logged in'
+    print("\nYou have successfully logged in!\n")
+    while True:
+        print("1. Balance")
+        print("2. Log out")
+        print("0. Exit")
+        login_success = int(input())
+        if login_success == 1:
+            balance()
+        elif login_success == 2:
+            print("\nYou have successfully logged out\n")
+            break
+        elif login_success == 0:
+            print("\nBye!")
+            sys.exit()
 
+def create_account():
+    '''number variable passes to the luhm_algo_checker function which return a valid card number and a pin code.
+    the card and pin number gets added into the database.'''
+    global number
+    number = luhm_algo_checker()
+    cur.execute("INSERT INTO card (number, pin) VALUES (?, ?)", (number[0], number[1]))
+    conn.commit()
+    print("\nYour card has been created")
+    print(f"Your card number:\n{number[0]}")
+    print(f"Your card PIN:\n{number[1]}\n")
+
+def main_menu():
+    "The main menu for creating an account."
+    while True:
+        print("1. Create an account")
+        print("2. Log into account")
+        print("0. Exit")
+        user_input = int(input())
+        if user_input == 1:
+            flag = True
+            while flag:
+                create_account()
+                flag = False
+        elif user_input == 2:
+            print("\nEnter your card number:")
+            card_number = input()
+            print("Enter your PIN: ")
+            pin_number = input()
+            if card_number in card_info and card_info[card_number] == pin_number:
+                logged_in()
+            else:
+                print("Wrong card number or PIN!")
+                True
+        elif user_input == 0:
+            print("\nBye!")
+            sys.exit()
+
+create_sql_table()
+main_menu()
